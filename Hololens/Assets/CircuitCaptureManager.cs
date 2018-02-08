737e9fd9346c8c75cb867ec375b37ba37468b867
mina yi <dearmeenuh@gmail.com>
2018-02-07 21:11:34 -0800

    adding unity project

---
+++ Hololens/Assets/CircuitCaptureManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.XR.WSA.WebCam;
+
+public class CircuitCaptureManager : MonoBehaviour {
+    [SerializeField]
+    private string[] keywords;
+    private KeywordRecognizer recognizer;
+
+    PhotoCapture photoCaptureObject = null;
+    Texture2D targetTexture = null;
+
+    // Use this for initialization
+    void Start () {
+        recognizer = new KeywordRecognizer(keywords);
+        recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+            photoCaptureObject = captureObject;
+            CameraParameters cameraParameters = new CameraParameters();
+            cameraParameters.hologramOpacity = 0.0f;
+            //cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            //cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+            // Activate the camera
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+                // Take a picture
+                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            });
+        });
+    }
+
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    {
+        photoCaptureFrame.UploadImageDataToTexture(targetTexture);
+        byte[] rawData = targetTexture.GetRawTextureData();
+
+        //send via http here
+    }
+        // Update is called once per frame
+        void Update () {
+		
+	}
+}
