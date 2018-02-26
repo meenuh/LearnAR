ea3af2c2afae34c9f36e99cea4dc6d0d7c3ae708
mina yi <dearmeenuh@gmail.com>
2018-02-26 15:41:39 -0800

    adding working circuit capture and display

--- Hololens/Assets/CircuitCaptureManager.cs
+++ Hololens/Assets/CircuitCaptureManager.cs
@@ -1,51 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Windows.Speech;
+﻿using UnityEngine;
 using UnityEngine.XR.WSA.WebCam;
+using HoloToolkit.Unity.InputModule;
 
 public class CircuitCaptureManager : MonoBehaviour {
-    [SerializeField]
-    private string[] keywords;
-    private KeywordRecognizer recognizer;
+    public int imageCount = 0;
 
     PhotoCapture photoCaptureObject = null;
     Texture2D targetTexture = null;
+    GameObject displayObject = null;
+    [SerializeField]
+    DisplayManager dsp = null;
+    
 
     // Use this for initialization
     void Start () {
-        recognizer = new KeywordRecognizer(keywords);
-        recognizer.OnPhraseRecognized += OnPhraseRecognized;
-        recognizer.Start();
+        //displayObject = new DisplayManager();
+        //displayObject = GameObject.FindGameObjectWithTag("CaptureManager");
     }
 
-    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    public void CaptureCircuitAndSave()
     {
+        //Resolution cameraResolution = PhotoCapture.SupportedResolutions;
+        Debug.Log("CaptureCircuitAndSave");
         PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
             photoCaptureObject = captureObject;
             CameraParameters cameraParameters = new CameraParameters();
             cameraParameters.hologramOpacity = 0.0f;
-            //cameraParameters.cameraResolutionWidth = cameraResolution.width;
-            //cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.cameraResolutionWidth = 896;
+            cameraParameters.cameraResolutionHeight = 504;
             cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
 
             // Activate the camera
             photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+                string filename = string.Format(@"CircuitImage{0}.jpg", imageCount);
+                string filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+
+                imageCount++;
                 // Take a picture
-                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+                photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToDisk);
+                //OnCapturedPhotoToDisk(result);
             });
         });
     }
 
-    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    void OnCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result)
     {
-        photoCaptureFrame.UploadImageDataToTexture(targetTexture);
-        byte[] rawData = targetTexture.GetRawTextureData();
+        //photoCaptureFrame.UploadImageDataToTexture(targetTexture);
+        //byte[] rawData = targetTexture.GetRawTextureData();
 
         //send via http here
+        Debug.Log("OnCapturedPhotoToDisk");
+        //if (displayObject.GetComponent<DisplayManager>() != null)
+        //{
+        //dsp = displayObject.GetComponent<DisplayManager>();
+        dsp.DisplayCircuit(imageCount - 1);
+        //}
+        //else if (dsp != null)
+        //{
+        //    dsp.DisplayCircuit(imageCount - 1);
+        //}
+        //possibly display image here?
+        //displayObject.DisplayCircuit(imageCount - 1);
+        //photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
     }
-        // Update is called once per frame
+
         void Update () {
 		
 	}
+
+    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        photoCaptureObject.Dispose();
+        photoCaptureObject = null;
+    }
 }
