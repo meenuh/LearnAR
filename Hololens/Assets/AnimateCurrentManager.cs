9268bc832f250453deff53a4e95ce45f4d3ad49d
mina yi <dearmeenuh@gmail.com>
2018-03-16 16:11:43 -0700

    adding code to display current flow animation

---
+++ Hololens/Assets/AnimateCurrentManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateCurrentManager : MonoBehaviour {
+    [SerializeField]
+    GameObject trailObj;
+    [SerializeField]
+    GameObject planeObj;
+    [SerializeField]
+    List<Transform> PointTransforms;
+    [SerializeField]
+    [Range(0f, 1f)]
+    float Period = 0.5f;
+
+
+    //void SetPoints(...) // use normalized coordinates
+    //{
+    //    PointTransforms =... // insantate new game objects
+    //    pt.parent = planeObj.transform; // parent them to the plane
+    //    pt.localPosition = ...
+    //}
+
+    void SetLocalPoints(IEnumerable<Vector3> local_points)
+    {
+        int node_number = 1;
+        foreach(var local_point in local_points)
+        {
+            var node = new GameObject("node" + node_number);
+            var node_trasform = node.transform;
+            node_trasform.SetParent(planeObj.transform);
+            node_trasform.localPosition = local_point;
+            node_number++;
+        }
+    }
+
+	// Use this for initialization
+	IEnumerator Start () {
+        //trailObj.transform.parent = planeObj.transform;
+        //trailObj.transform.position = planeObj.transform.position;
+        //trailObj.transform.SetParent(planeObj.transform, false);
+        //Hashtable ht = new Hashtable();
+
+        Hashtable opts = new Hashtable();
+        for (; ;)
+        {
+            foreach(var pt in PointTransforms)
+            {
+                opts.Add("easetype", iTween.EaseType.linear);
+                var local_pos = pt.localPosition;
+                var world_pos = planeObj.transform.TransformPoint(local_pos);
+                opts.Add("x", world_pos.x);
+                opts.Add("y", world_pos.y);
+                opts.Add("z", world_pos.z);
+                opts.Add("time", Period);
+                //iTween.MoveTo(trailObj, world_pos, Period);
+                iTween.MoveTo(trailObj, opts);
+                opts.Clear();
+                yield return new WaitForSeconds(Period);
+            }
+        }
+
+        //iTween.MoveTo(trailObj, ht);
+        //Hashtable test = new Hashtable();
+
+
+        //test.Add("x", 5);
+        //iTween.MoveTo(trailObj, test);
+        //test.Add("x", planeObj.transform.right);
+        //test.Add("y", planeObj.transform.left);
+        //test.Add("z", planeObj.transform.up);
+        //iTween.MoveTo(trailObj, test);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // should be called by display manager when it receives positions from backend
+    void ShowCurrentFlow()
+    {
+        
+    }
+}
---
+++ Hololens/Assets/DisplayManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayManager : MonoBehaviour {
+    [SerializeField]
+    public Texture2D circuitImage;
+    private RawImage img = null;
+    [SerializeField]
+    private TextMesh t;
+    [SerializeField]
+    GameObject plane;
+
+    public Material frontPlane;
+
+    // Use this for initialization
+    void Start () {
+        t.text = "shit happened";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    DisplayCircuit(0);
+        //}
+        //string filename = string.Format(@"CircuitImage{0}.jpg", circuitImageScript.imageCount - 1);
+        //string filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+    }
+
+    public void DisplayCircuit(int imageCount)
+    {
+        string filename = string.Format(@"CircuitImage{0}.jpg", imageCount);
+        string filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+
+        byte[] rawimage = File.ReadAllBytes(filePath);
+
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        foreach (byte b in rawimage)
+        {
+            sb.Append(b);
+        }
+        //t.text = sb.ToString();
+        t.text = "we did it";
+
+        circuitImage = new Texture2D(4, 4);
+        circuitImage.LoadImage(rawimage);
+        frontPlane.mainTexture = circuitImage;
+
+        // Apply to the plane
+        MeshRenderer renderer = plane.GetComponent<MeshRenderer>();
+        renderer.material = frontPlane;
+    }
+}
